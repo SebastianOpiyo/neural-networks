e7f1003f3311b03b29189a608662422b0b2f41be
SebastianOpiyo <sebamopale@gmail.com>
2020-08-07 03:23:56 +0300

    Add serializer and deserializer hooks.

--- utils/files_reading_writing.py
+++ utils/files_reading_writing.py
@@ -1,3 +1,9 @@
+import json
+import collections
+
+# Json has the ability to take bumpy objects and serializing them.
+# Bumpy objects include a tuple within a list, an nparray, etc.
+
 Jabberwocky = ["’Twas brillig, and the slithy toves",
                "Did gyre and gimble in the wade;",
                "All mimsy were the borogoves,",
@@ -35,14 +41,106 @@ Jabberwocky = ["’Twas brillig, and the slithy toves",
                ]
 
 # We are using the context manager "with" so we don't need to close the file.
-# When we get the program gets to end of the file it calls the exit func
-with open("Jab", "a") as f:
-    for line in Jabberwocky:
-        f.write(line + "\n")
+# When the program gets to end of the file it calls the exit func
 
-with open("Jab", "r") as f:
-    while True:
-        line = f.readline()
-        if not line:
-            break
-        print(line[:-1])
+"""Working without json.
+- has limitation especially when the data becomes really bumpy"""
+
+
+# the data structure is a real simple one
+def without_json():
+    with open("Jab", "a") as f:
+        for line in Jabberwocky:
+            f.write(line + "\n")
+
+    with open("Jab", "r") as f:
+        while True:
+            line = f.readline()
+            if not line:
+                break
+            print(line[:-1])
+
+
+flat = "I am sebastian"
+bumpy = ["Apple is sweet", "Did you try an Orange"]
+really_bumpy = ["A pancake", [2, 5, "A sausage"]]
+badly_bumpy = ["A pancake", ('a', 'azonto'), {'name': "Sebastiana",
+                                              3: "Deportivo", 8: "Man City"}, [2, 5, "A sausage"]]
+deque_data = collections.deque(badly_bumpy)
+
+
+# More bumpy data presented in here.
+# We are able to overcome most of the shortcomings with the use of json.
+def with_json():
+    with open("data.txt", "w") as f:
+        # this will fail with a deque.
+        # because type deque is not json serializable.
+        # we need to create json hooks to solve that problem.
+        json.dump(deque_data, f)
+
+    # with open("data.txt", "r") as f:
+    #     my_obj = json.load(f)
+    #     print(type(my_obj))
+    #     print(my_obj)
+
+
+"""A more complicated example with class objects encoding and decoding."""
+
+
+class Dog:
+    def __init__(self, name, age, tricks):
+        self._name = name
+        self._age = age
+        self._tricks = tricks
+
+    def __str__(self):
+        ret_str = f"{self._name} is {self._age} years old and knows"
+        ret_str += f"the following tricks. \n"
+        for trick in self._tricks:
+            ret_str += f"{trick} \n"
+        return ret_str
+
+
+fido = Dog("Fido", 10, ['sit', 'stay', 'roll over', 'Jump'])
+# print(fido)
+# returns the object we need.
+# print(fido.__dict__)
+
+
+class MultiTypeEncoder(json.JSONEncoder):
+    def default(self, o):
+        if isinstance(o, collections.deque):
+            return {"__deque__": list(o)}
+        elif isinstance(o, Dog):
+            return {"__Dog__": o.__dict__}
+        else:
+            json.JSONEncoder.default(o)
+
+
+def multi_type_decoder(o):
+    if "__deque__" in o:
+        return collections.deque(o["__deque__"])
+    if "__Dog__" in o:
+        dec_obj = o["__Dog__"]
+        name = dec_obj["_name"]
+        age = dec_obj["_age"]
+        tricks = list(dec_obj["_tricks"])
+        ret_obj = Dog(name, age, tricks)
+        return ret_obj
+    else:
+        return o
+
+
+with open("data.txt", "w") as f:
+    # We add the json hook to enable it works.
+    json.dump(fido, f, cls=MultiTypeEncoder)
+
+with open("data.txt", "r") as f:
+    my_obj = json.load(f, object_hook=multi_type_decoder)
+    print(type(my_obj))
+    print(my_obj)
+
+
+# if __name__ == '__main__':
+#     without_json()
+#     # with_json()
